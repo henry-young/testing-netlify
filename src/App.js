90e4a34e17c2e86666dbdfa84c56d07412d56d7a
Henry Young <henry.young@industrieit.com>
2017-03-29 10:01:07 +1100

    removed header in dev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const ENVIRONMENT = process.env.REACT_APP_ENV || 'not found';
@@ -8,10 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <h1>I Deleted the header!</h1>
         <p className="App-intro">
           I am the development environment and I live on another branch.
         </p>
