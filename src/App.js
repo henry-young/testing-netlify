376f72e3b9d0daacf6026061e6ffbd815d636dd9
Henry Young <henry.young@industrieit.com>
2017-03-28 13:57:32 +1100

    Dev branch changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          I am the development environment and I live on another branch.
         </p>
         <p>
           Current environment: {ENVIRONMENT}
